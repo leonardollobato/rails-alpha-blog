198595ba9ab143640babc164ce8afff6c3b378c4
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-06-24 20:43:03 -0300

    added bcrypt, hassecurepassword to user and password digest field

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence:true, length: {minimum:3, maximum:105}, uniqueness: {case_sensitive:false},format:{with:VALID_EMAIL_REGEX}
 
+	has_secure_password
 end
